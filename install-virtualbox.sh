b8ed7e0d325b1413e052c9a53928da7a8560b1c9
顏成翰 <hans@smartfun.com.tw>
2020-04-01 14:02:53 +0800

    modified vm 2

--- install-virtualbox.sh
+++ install-virtualbox.sh
@@ -4,8 +4,8 @@ cd /tmp
 echo Downloading Virtual Box...
 curl -O http://download.virtualbox.org/virtualbox/5.0.12/VirtualBox-5.0.12-104815-OSX.dmg
 echo Installing Virtual Box...
-/usr/local/bin/hdiutil attach VirtualBox-5.0.12-104815-OSX.dmg
-/usr/local/bin/installer -pkg /Volumes/VirtualBox/VirtualBox.pkg -target /Volumes/Macintosh\ HD
+/hdiutil attach VirtualBox-5.0.12-104815-OSX.dmg
+/installer -pkg /Volumes/VirtualBox/VirtualBox.pkg -target /Volumes/Macintosh\ HD
 rm VirtualBox-5.0.12-104815-OSX.dmg
 echo VirtualBox Installed.
 cd ${saved_dir}
